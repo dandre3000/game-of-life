81ca5ed7ef6fed094f11986e3ba3ef1bd9314314
dandre3000 <deaundre.payne@gmail.com>
2021-01-22 22:54:37 -0500

    Add Move, Replicator & Brain's Brain

--- src/automata.js
+++ src/automata.js
@@ -6,9 +6,19 @@ export const rules = {
 		S: [2, 3],
 		G: 2
 	},
+	'Move': {
+		B: [3, 6, 8],
+		S: [2, 4, 5],
+		G: 2
+	},
 	'Day & Night': {
 		B: [3, 6, 7, 8],
 		S: [3, 4, 6, 7, 8],
+		G: 2
+	},
+	'Replicator': {
+		B: [1, 3, 5, 7],
+		S: [1, 3, 5, 7],
 		G: 2
 	},
 	'Seeds': {
@@ -20,6 +30,11 @@ export const rules = {
 		B: [4, 5, 8],
 		S: [0, 1, 2, 3, 4, 5],
 		G: 7
+	},
+	'Brian\'s Brain': {
+		B: [2],
+		S: [],
+		G: 3
 	},
 	'Star Wars': {
 		B: [2],
